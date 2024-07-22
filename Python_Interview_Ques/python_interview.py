fa515b112b048496a7508092aa6521a943026ca2
smarni4 <srimarni147@gmail.com>
2024-07-22 11:14:28 -0400

    minor

--- Python_Interview_Ques/python_interview.py
+++ Python_Interview_Ques/python_interview.py
@@ -399,3 +399,30 @@ x = np.array([[1, 2], [3, 4], [5, 6], [7, 8], [9, 10]])
 y = np.array([4, 8, 2, 9, 11])
 model = random_forest(x, y)
 print(f"Feature importance : {model.feature_importances_}")
+
+
+from sklearn.datasets import load_iris
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+
+iris = load_iris()
+X = iris.data
+y = iris.target
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+clf = DecisionTreeClassifier(criterion="entropy", max_depth=3, random_state=42)
+
+clf.fit(X_train, y_train)
+
+y_pred = clf.predict(X_test)
+
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(y_test, y_pred)
+print(f"Accuracy: {accuracy}")
+
+from sklearn.tree import plot_tree
+
+img = plot_tree(clf, filled=True, rounded=True, class_names=iris.target_names)
+print(img)
+
